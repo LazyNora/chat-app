fac243d76a786d960b895bba531ac7d5e36aef6a
LazyNora <lazynora@outlook.com>
2025-09-03 14:17:33 +0700

    clean up the public directory and setup shadcn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	images: {
+		domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
+	},
 };
 
 export default nextConfig;
